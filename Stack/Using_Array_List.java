d90a7c13e705ea82c43a0e567373628e197ab07f
shivkumar9693 <shivkumarthakur9693@gmail.com>
2025-02-06 20:09:20 +0530

    added stack folder

---
+++ Stack/Using_Array_List.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+
+public class Using_Array_List {
+
+    static class Stack {
+        static ArrayList<Integer> list = new ArrayList<>();
+
+        // is empty
+        public static boolean isEmpty() {
+            return list.size() == 0;
+        }
+
+        // push
+        public static void push(int data) {
+            list.add(data);
+        }
+
+        // pop
+        public static int pop() {
+            int top = list.get(list.size() - 1);
+            list.remove(list.size() - 1);
+            return top;
+        }
+
+        // peek
+        public static int peek() {
+            int top = list.get(list.size() - 1);
+            return top;
+        }
+    }
+
+    public static void main(String[] args) {
+        Stack s = new Stack();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        while (!s.isEmpty()) {
+            System.out.println(s.pop());
+        }
+
+    }
+}
